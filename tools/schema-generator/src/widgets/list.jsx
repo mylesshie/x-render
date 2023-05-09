927cc6cff657c09a8c60473beaaf5d0e7074dbd3
Will.xie <mylesshie@gmail.com>
2023-05-09 16:24:31 +0800

    feat: beat 15版本修改

--- tools/schema-generator/src/widgets/list.jsx
+++ tools/schema-generator/src/widgets/list.jsx
@@ -1,5 +1,4 @@
 import { DeleteOutlined, PlusCircleOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -13,18 +12,7 @@ export default function list(props) {
     <div className="flex flex-column">
       <div className="fr-set w-100 flex flex-column ba pt4 pb2 ph2 relative b--black-10">
         {props.children}
-        <Button
-          size="small"
-          className="self-end"
-          type="dashed"
-          icon={<DeleteOutlined />}
-        >
-          {t("删除")}
-        </Button>
       </div>
-      <Button size="small" className="self-end" icon={<PlusCircleOutlined />}>
-        {t("添加")}
-      </Button>
     </div>
   );
 }
